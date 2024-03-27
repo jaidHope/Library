9344b1a9e548d789fcd2bee7018499c8a3ac97ab
Jaid Heesen <jaid.heesen@accso.de>
2024-03-27 10:34:30 +0200

    Added Spring REST docs based on LibraryControllerTests

--- src/test/java/com/example/Library/LibraryControllerTests.java
+++ src/test/java/com/example/Library/LibraryControllerTests.java
@@ -2,7 +2,9 @@ package com.example.Library;
 
 import com.example.Library.service.BookService;
 import org.junit.jupiter.api.Test;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -10,6 +12,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets")
 @WebMvcTest(LibraryController.class)
 class LibraryControllerTests {
 
@@ -20,17 +24,24 @@ class LibraryControllerTests {
     private BookService bookService;
 
     @Test
+    void contextLoads() throws Exception {
+        //sanity test
+    }
+
+    @Test
     void testGetAllBooks() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/books")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("getAllBooks"));
     }
 
     @Test
     void testGetBookById() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/books/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("getBookById"));
     }
 
     @Test
@@ -38,7 +49,8 @@ class LibraryControllerTests {
         mockMvc.perform(MockMvcRequestBuilders.post("")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\":\"Test Book\",\"author\":\"Test Author\",\"description\":\"Test Description\"}"))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(document("addBook"));
     }
 
     @Test
@@ -46,12 +58,14 @@ class LibraryControllerTests {
         mockMvc.perform(MockMvcRequestBuilders.put("/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\":\"Updated Book\",\"author\":\"Updated Author\",\"description\":\"Updated Description\"}"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(document("updateBook"));
     }
 
     @Test
     void testDeleteBook() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/{id}", 1))
-                .andExpect(MockMvcResultMatchers.status().isNoContent());
+                .andExpect(MockMvcResultMatchers.status().isNoContent())
+                .andDo(document("deleteBook"));
     }
 }
